cb160c10dd3133deb95ff77f418f1b157e711be4
pirana <21ug0272@sltc.ac.lk>
2024-01-11 00:48:36 +0530

    21ug0272

---
+++ app/src/main/java/com/example/tipcalculator01/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.tipcalculator01
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+
+class MainActivity : AppCompatActivity() {
+
+    private TextView
+    private Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
